8e33b154dcee845f3c186c88057624598aa35610
Satyam Singh <satyamsingh5august@gmail.com>
2025-01-27 11:14:20 +0000

    go init

---
+++ 02variables/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "John Doe";
+	fmt.Println("Hello, " + username + "!");
+	fmt.Printf("Variable of Type: %T \n", username);
+	
+	var isLoggedIn bool = true;
+	fmt.Println("Is logged in: ", isLoggedIn);
+	fmt.Printf("Variable of Type: %T \n", isLoggedIn);
+
+	var smallVal int  = 255;
+	fmt.Println("Is logged in: ", smallVal);
+	fmt.Printf("Variable of Type: %T \n", smallVal);
+
+	testNum := 10;
+	fmt.Println("Test Number: ", testNum);
+}
